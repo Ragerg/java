8a4a5eb61cdfea89bff4b51b71d55d5f327923d1
Ragerg <126847907+Ragerg@users.noreply.github.com>
2023-03-08 23:33:14 +0900

    Revert "복습"

--- src/kr/ac/kopo/day02/VariableMain02.java
+++ src/kr/ac/kopo/day02/VariableMain02.java
@@ -2,26 +2,31 @@ package kr.ac.kopo.day02;
 
 public class VariableMain02 {
 	
-	public static void main(String[] ars) {
+	public static void main(String[] args) {
 		
 		double num = 12.34;
-		System.out.println("num1 : " + num);
+		
+		//형변환 : 내가 가지고 있는 타입을 잠시동안 다른 타입으로 인식하게 만드는 것
+		num = /*(double)*/34;    // 묵시적형변환 : 생략할 수 있는 형변환(작은 것을 큰 것에)
+				
+		System.out.println("num : " + num);
 		
 		int num2;
-		num2 = (int)45.67;
+		num2 = (int)45.67;    // 강제적(명시적)형변환
+		
 		System.out.println("num2 : " + num2);
 		
-		float num3;
-		num3 = (float)10.4;
-		System.out.println("num3 : " + num3);
-		
-		float num4;
-		num4 = 25.36f;
-		System.out.println("num4 : " + num4);
-		
-		System.out.println("num5 : " + (12 + (int)12.34));
-		System.out.println("num6 : " + (12 + 12.34));
-		
+	    float num3;
+	    num3 = (float)10.4;
+	    num3 = 10.4f;    // =(float)10.4;
+	    
+	    
+	    System.out.println("num3 : " + num3);
+	    
+	    System.out.println(12 + (int)12.34);
+	    System.out.println(/*(double)*/12 + 12.34);
+	    
+	    
 	}
 
 }
