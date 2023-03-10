04c3bfe0645659cdb406f9af7cfbadb9c0ffa348
Lee Nayeon <in6306174@gmail.com>
2023-03-10 10:28:17 +0900

    z

--- src/kr/ac/kopo/day04/OperationMain05.java
+++ src/kr/ac/kopo/day04/OperationMain05.java
@@ -1,5 +1,12 @@
 package kr.ac.kopo.day04;
 
 public class OperationMain05 {
+	
+	public static void main(String[] args) {
+		
+		int num = 0x0055ff00;
+		System.out.printf("%x\n" , num);
+		
+	}
 
 }
