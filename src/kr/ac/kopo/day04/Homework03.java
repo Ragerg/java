abc034d5e95a8ce250904eb89b36ca355ecc85c6
Lee Nayeon <in6306174@gmail.com>
2023-03-10 18:56:33 +0900

    작은 수에서 큰 수로 수정

--- src/kr/ac/kopo/day04/Homework03.java
+++ src/kr/ac/kopo/day04/Homework03.java
@@ -15,15 +15,15 @@ public class Homework03 {
 		System.out.print("세 번째 정수 : ");
 		int no3 = sc.nextInt();
 		
-		if(no1 > no2 && no2 > no3) {
+		if(no1 < no2 && no2 < no3) {
 			System.out.println(no1 + "," + no2 + "," + no3);
-		} else if(no1 > no2 && no3 > no2 && no1 > no3) {
+		} else if(no1 < no2 && no3 < no2 && no1 < no3) {
 			System.out.println(no1 + "," + no3 + "," + no2);
-		} else if(no2 > no1 && no1 >  no3) {
+		} else if(no2 < no1 && no1 <  no3) {
 			System.out.println(no2 + "," + no1 + "," + no3);
-		} else if(no2 > no1 && no3 >  no1 && no2 > no3) {
+		} else if(no2 < no1 && no3 <  no1 && no2 < no3) {
 			System.out.println(no2 + "," + no3 + "," + no1);
-		} else if(no3 > no1 && no1 >  no2) {
+		} else if(no3 < no1 && no1 <  no2) {
 			System.out.println(no3 + "," + no1 + "," + no2);
 		} else  {
 			System.out.println(no3 + "," + no2 + "," + no1);
