0b8aed16dbac0953c506625b6b5dd6e01f9069df
Lee Nayeon <in6306174@gmail.com>
2023-04-05 16:10:42 +0900

    DAY19

--- src/kr/ac/kopo/day19/Homework/Menu.java
+++ src/kr/ac/kopo/day19/Homework/Menu.java
@@ -12,7 +12,7 @@ public class Menu {
 		try {
 			Print.print(); // 현재 경로의 내용 출력
 		} catch (Exception e) { // 없는 디렉토리 입력 시 출력
-			System.out.println("!!!제대로 입력하세요!!!");
+			System.out.println("없는 폴더입니다.");
 			highDir(); // 다시 상위 디렉토리로 이동
 			menu();
 			
@@ -33,7 +33,9 @@ public class Menu {
 		case 'R' :
 			this.reName();
 			break;
-				
+		default:
+			System.out.println("종료하겠습니다");
+			System.exit(0);		
 		}
 		menu();
 		
@@ -53,7 +55,11 @@ public class Menu {
 	
 	public void highDir() {
 		File high = new File(Print.getPath()); // 현재 경로를 가진 새로운 파일객체 생성
-		Print.setPath(high.getParent()); // 새로운 파일객체의 상위 경로로 이동
+		if(high.getParent() != null) {
+			Print.setPath(high.getParent()); // 새로운 파일객체의 상위 경로로 변경
+		} else {
+			System.out.println("없는 경로입니다.");
+		}
 	}
 	
 	public void reName() {
@@ -67,7 +73,11 @@ public class Menu {
 		boolean result = origin.renameTo(rename); // 이름 변경
 		if(result) {
 			System.out.println("이름이 변경 되었습니다");
+		} else {
+			System.out.println("이름이 변경에 실패했습니다");
+			
 		}
+	
 		
 		
 		
